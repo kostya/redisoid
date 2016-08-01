67f31791b2ed2c59c4a3b32f2d3f2a8f27154e9e
Konstantin Makarchev <kostya27@gmail.com>
2016-08-01 19:58:14 +0400

    better example

--- example2.cr
+++ example2.cr
@@ -4,15 +4,27 @@ client = Redisoid.new(host: "localhost", port: 6379, pool: 150)
 
 client.del("queue")
 
+c = 0
+
 100.times do
   spawn do
     loop do
       client.lpush("queue", "abc")
-      sleep rand(0.01)
+      sleep 0.01
+    end
+  end
+
+  spawn do
+    loop do
+      if res = client.lpop("queue")
+        c += res.size
+      else
+        sleep 0.01
+      end
     end
   end
 end
 
 sleep 5.0
-p client.llen("queue")
+p c
 client.del("queue")
