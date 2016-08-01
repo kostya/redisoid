39e8fb52cf1508899e07dff94d35fee2cc003154
Konstantin Makarchev <kostya27@gmail.com>
2016-08-01 19:31:16 +0400

    add specs

--- spec/redisoid_spec.cr
+++ spec/redisoid_spec.cr
@@ -1,9 +1,46 @@
 require "./spec_helper"
 
-describe Redisoid do
-  # TODO: Write tests
+# manualy run: redis-server --port 7777 --timeout 2
+CONFIG  = {host: "localhost", port: 7777}
+TIMEOUT = 2
 
-  it "works" do
-    false.should eq(true)
+describe Redisoid do
+  it "standard client" do
+    client = Redis.new(**CONFIG)
+    client.set("bla1", "a")
+    client.get("bla1").should eq "a"
+
+    sleep(TIMEOUT + 1.0)
+
+    expect_raises(Redis::DisconnectedError) do
+      client.get("bla1")
+    end
+  end
+
+  it "reconnect client" do
+    client = Redisoid.new(**CONFIG)
+    client.set("bla1", "a")
+    client.get("bla1").should eq "a"
+
+    sleep(TIMEOUT + 1.0)
+
+    client.get("bla1").should eq "a"
+  end
+
+  it "reconnect method with block" do
+    client1 = Redisoid.new(**CONFIG)
+    client2 = Redisoid.new(**CONFIG)
+    ch = Channel(String).new
+    spawn do
+      client2.subscribe("sub_test") do |on|
+        on.message do |_, msg|
+          ch.send(msg)
+        end
+      end
+    end
+    sleep 0.1
+    client1.publish("sub_test", "bla")
+
+    ch.receive.should eq "bla"
   end
 end
